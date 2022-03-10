58f35a66d6b16bad7e9cae8bf2530e9f27d74d3a
宋宇欣 <kiko@xiaohongshu.com>
2022-03-10 16:58:20 +0800

    update

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 python main.py --model_name "MIMO-UNetPlus" \
 --mode "train" \
 --data_dir "dataset/GOPRO" \
---batch_size 16 \
---learning_rate 2e-4 \
+--batch_size 64 \
+--learning_rate 4e-4 \
 --print_freq 10 \
 --resume results/MIMO-UNetPlus/weights/model_200.pkl \
 --gpus 0,1,2,3
